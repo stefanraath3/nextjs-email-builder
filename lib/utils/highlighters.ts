d55fe5c7b158fb04e861c226f3d011ea28ff6672
Stefan Raath <stefanraath3@gmail.com>
2025-10-27 20:55:36 +0200

    Refactor highlight functions to use updated Prettier imports for improved formatting and plugin support.

--- lib/utils/highlighters.ts
+++ lib/utils/highlighters.ts
@@ -1,15 +1,18 @@
 import hljs from "highlight.js/lib/core";
 import json from "highlight.js/lib/languages/json";
 import xml from "highlight.js/lib/languages/xml";
-import { format } from "prettier";
+import prettier from "prettier/standalone";
+import prettierPluginEstree from "prettier/plugins/estree";
+import prettierPluginHtml from "prettier/plugins/html";
 
 hljs.registerLanguage("json", json);
 hljs.registerLanguage("html", xml);
 
 export async function highlightHtml(value: string): Promise<string> {
   try {
-    const prettyValue = await format(value, {
+    const prettyValue = await prettier.format(value, {
       parser: "html",
+      plugins: [prettierPluginHtml, prettierPluginEstree],
     });
     return hljs.highlight(prettyValue, { language: "html" }).value;
   } catch (e) {
@@ -20,8 +23,9 @@ export async function highlightHtml(value: string): Promise<string> {
 
 export async function highlightJson(value: string): Promise<string> {
   try {
-    const prettyValue = await format(value, {
+    const prettyValue = await prettier.format(value, {
       parser: "json",
+      plugins: [prettierPluginHtml, prettierPluginEstree],
     });
     return hljs.highlight(prettyValue, { language: "json" }).value;
   } catch (e) {
